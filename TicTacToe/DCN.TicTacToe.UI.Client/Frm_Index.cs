ff685d6052919b02935400db5d31bbcb8e02e73c
nguyendcn <nguyendangdcn@gmail.com>
2019-09-25 22:08:26 +0700

    Design model form client.

---
+++ TicTacToe/DCN.TicTacToe.UI.Client/Frm_Index.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DCN.TicTacToe.Client;
+
+namespace DCN.TicTacToe.UI.Client
+{
+    public partial class Frm_Index : Form
+    {
+        #region Properties for title bar
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,
+                         int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        #endregion
+
+        private DCN.TicTacToe.Client.Client client;
+
+        public Frm_Index()
+        {
+            client = new DCN.TicTacToe.Client.Client();
+            InitializeComponent();
+        }
+
+        #region Set up for title bar
+        private void pnl_titleBar_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+
+        private void btn_Maximise_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                return;
+            }
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btn_Minimise_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        #endregion
+
+        private void btn_ConnectToServer_Click(object sender, EventArgs e)
+        {
+            client.Connect("localhost", 9999);
+
+        }
+
+        private void Status(String str)
+        {
+            InvokeUI(() => {  str+=""; });
+        }
+
+        private void InvokeUI(Action action)
+        {
+            this.Invoke(action);
+        }
+    }
+}
--- TicTacToe/DCN.TicTacToe.UI.Client/Program.cs
+++ TicTacToe/DCN.TicTacToe.UI.Client/Program.cs
@@ -16,7 +16,7 @@ namespace DCN.TicTacToe.UI.Client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frm_Index());
         }
     }
 }
